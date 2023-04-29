01669752035f65c740ae884326176a20891975de
22P31A0441 <119719043+22P31A0441@users.noreply.github.com>
2023-04-29 10:15:06 +0530

    import java.util.Scanner; public class AreaofCircle {     public static void main(String args[])     {         int radius;         double pi=3.14,area;         Scanner sc=new Scanner(System.in);         radius =sc.nextInt();         area=pi*radius*radius;         System.out.format("%.2f",area);     } }

--- Area_of_a_circle.java
+++ Area_of_a_circle.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
-public class Circle
+public class AreaofCircle
 {
     public static void main(String args[])
     {
-         int radius;
-        double pi = 3.14, area;
+        int radius;
+        double pi=3.14,area;
         Scanner sc=new Scanner(System.in);
-        radius=sc.nextInt();
-        area = pi * radius * radius;
+        radius =sc.nextInt();
+        area=pi*radius*radius;
         System.out.format("%.2f",area);
     }
 }
