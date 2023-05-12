358efdc5948798eb1b059b6b71b080efce0f7d4a
22P31A0441 <119719043+22P31A0441@users.noreply.github.com>
2023-05-12 20:33:26 +0530

    import java.util.Scanner; public class Color {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         char ch;         ch=sc.nextLine().charAt(0);         if(ch=='v'||ch=='V')         {             System.out.println("violet");         }       else if(ch=='i'||ch=='I')         {             System.out.println("Indigo");         }        else if(ch=='b'||ch=='B')         {             System.out.println("Blue");         }        else if(ch=='g'||ch=='G')         {             System.out.println("Green");         }        else if(ch=='y'||ch=='Y')         {             System.out.println("Yellow");         }        else if(ch=='o'||ch=='O')         {             System.out.println("Orange");         }        else if(ch=='r'||ch=='R')         {             System.out.println("Red");         }         else         {             System.out.println("-1");         }     } }

---
+++ Color_Code.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Color
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch;
+        ch=sc.nextLine().charAt(0);
+        if(ch=='v'||ch=='V')
+        {
+            System.out.println("violet");
+        }
+      else if(ch=='i'||ch=='I')
+        {
+            System.out.println("Indigo");
+        }
+       else if(ch=='b'||ch=='B')
+        {
+            System.out.println("Blue");
+        }
+       else if(ch=='g'||ch=='G')
+        {
+            System.out.println("Green");
+        }
+       else if(ch=='y'||ch=='Y')
+        {
+            System.out.println("Yellow");
+        }
+       else if(ch=='o'||ch=='O')
+        {
+            System.out.println("Orange");
+        }
+       else if(ch=='r'||ch=='R')
+        {
+            System.out.println("Red");
+        }
+        else
+        {
+            System.out.println("-1");
+        }
+    }
+}
