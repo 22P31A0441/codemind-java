94e0deffc8a9de468d51da24c100b80bed2e364c
22P31A0441 <119719043+22P31A0441@users.noreply.github.com>
2023-05-17 20:45:57 +0530

    import java.util.Scanner; public class Sample {     public static void main(String arg[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         if(n<3)         System.out.println(n-1);         else         {             System.out.println("2");             sc.close();         }     }
    }

---
+++ The_Pandemic.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Sample
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if(n<3)
+        System.out.println(n-1);
+        else
+        {
+            System.out.println("2");
+            sc.close();
+        }
+    }
+    
+}
