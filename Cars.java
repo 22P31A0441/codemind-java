dc9ceea6204b812abefcaf184ed9813775cd1129
22P31A0441 <119719043+22P31A0441@users.noreply.github.com>
2023-05-22 21:43:37 +0530

    import java.util.Scanner; public class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         import java.util.Scanner; public class Sample {     public static void main(String args[])     {         int a;         String Sound="";         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         if(a%3==0)         Sound=Sound+"Pling";         if(a%5==0)         Sound=Sound+"Plang";         if(a%7==0)         Sound+="Plong";         if(Sound.isEmpty())         System.out.println(a);         else         System.out.println(Sound);         sc.close();
        }
        
    }
                while(d+s1>=s2)
                {
                    c++;
                    s1=s1+n1;
                    s2=s2+n2;
                }
                System.out.println(c);
                }
                sc.close();
            }
            
    }

---
+++ Cars.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        import java.util.Scanner;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        int a;
+        String Sound="";
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        if(a%3==0)
+        Sound=Sound+"Pling";
+        if(a%5==0)
+        Sound=Sound+"Plang";
+        if(a%7==0)
+        Sound+="Plong";
+        if(Sound.isEmpty())
+        System.out.println(a);
+        else
+        System.out.println(Sound);
+        sc.close();
+        
+    }
+    
+}
+            while(d+s1>=s2)
+            {
+                c++;
+                s1=s1+n1;
+                s2=s2+n2;
+            }
+            System.out.println(c);
+            }
+            sc.close();
+        }
+        
+}
